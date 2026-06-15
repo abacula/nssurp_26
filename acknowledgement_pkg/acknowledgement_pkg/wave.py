8d40a60df5d55f5c8e78f98ca321dcc60b5a3f18
abacula <alexandra.bacula@plu.edu>
2026-06-15 14:18:03 -0700

    Fixed run_away and wave nodes

--- acknowledgement_pkg/acknowledgement_pkg/wave.py
+++ acknowledgement_pkg/acknowledgement_pkg/wave.py
@@ -9,16 +9,23 @@ from geometry_msgs.msg import Twist
 # Decide your node class name
 class Wave(Node):
     def __init__(self):
+
+        self.saw_person = False
+
         # Change to have your node name
         super().__init__('wave_node')
         self.has_seen = False
 
         self.publisher = self.create_publisher(Twist, '/robot4/cmd_vel_unstamped', 10)
         self.ack_sub = self.create_subscription(HallwayAck, '/robot4/hallway_ack', self.hallway_cb, 10)
+
+        self.timer = self.create_timer(0.1, self.loop)
+
     
     def hallway_cb(self, msg):
 
-        if msg.person_detected:
+        if msg.person_detected and msg.bbox_height > 80:
+            self.saw_person = True
             self.wave()
 
     def wave(self):
@@ -30,9 +37,9 @@ class Wave(Node):
         self.publisher.publish(twist)
         time.sleep(1)
         
-        twist.angular.z = -5.0
+        twist.angular.z = -10.0
         self.publisher.publish(twist)
-        time.sleep(2)
+        time.sleep(1)
 
         twist.angular.z = 5.0
         self.publisher.publish(twist)
@@ -40,7 +47,16 @@ class Wave(Node):
         
         twist.angular.z = 0.0
         self.publisher.publish(twist)
+        
+        self.saw_person = False
 
+    def loop(self):
+        
+        twist = Twist()
+        
+        if not self.saw_person:
+            twist.linear.x = 0.5
+            self.publisher.publish(twist)
 
 def main(args=None):
     rclpy.init(args=args)
